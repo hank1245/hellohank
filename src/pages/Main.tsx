c420c1aed7fb3ae2f01db778b8f1b505f192846a
Hank Kim <hank29206880@gmail.com>
2023-03-02 20:58:08 +0900

    Likes drag prevent

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -2,6 +2,7 @@ import React, { Children } from "react";
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls } from "@react-three/drei";
 import Model from "../components/Model";
+import styled from "styled-components";
 
 interface Props {}
 
@@ -22,8 +23,23 @@ const Main = () => {
         <Model position={[0.025, -0.9, 0]} />
         <OrbitControls />
       </Canvas>
+      <Likes>Likes</Likes>
     </>
   );
 };
+
+const Likes = styled.div`
+  position: absolute;
+  bottom: 300px;
+  left: 100px;
+  color: white;
+  font-size: 2rem;
+  -webkit-touch-callout: none;
+  -webkit-user-select: none;
+  -khtml-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+`;
 
 export default Main;
